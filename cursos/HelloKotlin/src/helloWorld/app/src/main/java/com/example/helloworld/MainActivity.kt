953c1d65df22fb9f1d34ecbd048d2e27bd7fa4a6
Matheus Lima <matheus.correa@mindtek.com.br>
2021-02-09 00:05:33 -0300

    contador de click

--- cursos/HelloKotlin/src/helloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ cursos/HelloKotlin/src/helloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn = findViewById<Button>(R.id.btn_login)
+        var texto = findViewById<TextView>(R.id.textView)
+        var count : Int = 0
+        btn.setOnClickListener {
+            count++
+            texto.setText(count.toString())
+        }
         //UMA FORMA DE CRIAR OBJETOS:
         //val texto = TextView(this)
         //texto.text = "Hello world"
